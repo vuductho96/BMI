f64bfac1ba996e861218b15d9d33265864edf384
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-04-01 10:11:29 +0700

    BMI

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace BMI;
+class BMI{
+static void Main(string[] args){
+double height ,weight;
+Console.WriteLine("Hay nhap can nang=");
+height=float.Parse(Console.ReadLine());
+Console.WriteLine("Hay nhap chieu cao");
+weight=float.Parse(Console.ReadLine());
+double bmi = weight / Math.Pow(height,2);
+            bmi = Math.Round(bmi, 1);
+  Console.Write("BMI: "+ bmi);
+
+            if (bmi < 18)
+                Console.WriteLine(" Underweight");
+            else if (bmi < 25.0)
+                Console.WriteLine(" Normal");
+            else if (bmi < 30.0)
+                Console.WriteLine(" Overweight");
+            else
+                Console.WriteLine(" Obese");
+}
+
+
+}
